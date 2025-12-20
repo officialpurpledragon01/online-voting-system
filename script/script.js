4a26064f16ed8f7311daffbbdb41ea6fcbdffcfb
purple dragon <officialpurpledragon01@gmail.com>
2025-12-20 09:18:02 +0100

    fixed route issue

--- script/script.js
+++ script/script.js
@@ -1,25 +1,7 @@
-import {users} from '../data/users.js'
-
 document.querySelector('.logIn').addEventListener('click',()=>{
-  window.location.href = '../pages/login.html'
+  window.location.href = 'pages/login.html'
 });
 
 document.querySelector('.getStart').addEventListener('click',()=>{
-  window.location.href = '../pages/signup.html'
+  window.location.href = 'pages/signup.html'
 });
-
-let passwords = '';
-// let index = 0
-
-users.map((user,index) => {
-  let pasword = user.password
-  // index += 1
-
-  passwords += `
-    <p>
-      ${index}: ${pasword}
-    </p>
-  `
-})
-
-console.log(passwords);
